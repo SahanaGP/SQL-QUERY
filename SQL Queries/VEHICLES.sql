dd1ad18137589720fd9302a23587899a4fab68f8
SahanaGP <sahanagp.xworkz@gmail.com>
2021-11-16 09:24:40 +0530

    Vehicle sql query

--- SQL Queries/VEHICLES.sql
+++ SQL Queries/VEHICLES.sql
@@ -115,8 +115,93 @@ SELECT Bike_Model,COUNT(Bike_Model) AS No_of_bikes_in_Model FROM bike_details GR
 
 SELECT Bike_Price,COUNT(Bike_Price) AS No_of_bikes_in_Price FROM bike_details WHERE Bike_Price>80000 GROUP BY Bike_Brand;
 
+SELECT Bike_Model,COUNT(Bike_Model) AS No_of_bikes FROM bike_details GROUP BY Bike_Model;
 
+SELECT Bike_Brand,MAX(Bike_Price) AS Highest_cost_of_bike FROM bike_details GROUP BY Bike_Brand;
 
+SELECT Bike_Brand,MIN(Bike_Price) AS Lowest_cost_of_bike FROM bike_details GROUP BY Bike_Brand;
+
+SELECT Bike_Id,Bike_Brand,SUM(Bike_Price)AS Total_Price FROM bike_details GROUP BY Bike_Id,Bike_Brand HAVING Total_Price>70000 ;
+
+SELECT Bike_Id,Bike_Brand,MIN(Bike_Price)AS Lowest_price,MAX(Bike_Price)AS Highest_price FROM bike_details GROUP BY Bike_Id,Bike_Brand HAVING Bike_Id>2;
+
+SELECT Engine_No,Bike_Model,COUNT(Bike_Color)AS no_of_copies FROM bike_details GROUP BY Engine_No,Bike_Model HAVING no_of_copies>0;
+
+SELECT Bike_Brand,MAX(Bike_Price)AS Highest_price FROM bike_details GROUP BY Bike_Brand HAVING Highest_price>80000; 
+SELECT Bike_Brand,MIN(Book_Price)AS Lowest_price FROM bike_details GROUP BY Bike_Brand HAVING Lowest_price>80000;
+
+SET AUTOCOMMIT=1;
+
+SELECT*FROM bike_details;
+
+DELETE FROM bike_details WHERE Bike_Id=4;
+
+COMMIT;
+
+DELETE FROM bike_details WHERE Bike_Id=3;
+
+DELETE FROM bike_details WHERE Bike_Model='Dio';
+
+SAVEPOINT Bike_Model;
+
+UPDATE bike_details SET Bike_Price =90000 WHERE Bike_Id=2;
+
+UPDATE bike_details SET Bike_Price =99000 WHERE Bike_Id=3;
+
+SAVEPOINT Bike_Id;
+
+ROLLBACK;
+
+SELECT MAX(Bike_Price) FROM  bike_details;
+SELECT MIN(Bike_Price) FROM  bike_details;
+
+-- get the details  of the bike which has the highest and lowest cost
+
+SELECT*FROM  bike_details WHERE Bike_Price =(SELECT MAX(Bike_Price) FROM  bike_details);
+
+SELECT*FROM  bike_details WHERE Bike_Price =(SELECT MIN(Bike_Price) FROM  bike_details);
+
+SELECT*FROM  bike_details WHERE Bike_Price <(SELECT MAX(Bike_Price) FROM  bike_details);
+
+SELECT*FROM  bike_details WHERE Bike_Price >(SELECT MIN(Bike_Price) FROM  bike_details);
+
+-- get second largest cost of bike 
+
+SELECT*FROM bike_details WHERE Bike_Price=80000;
+SELECT MAX(Bike_Price)AS Second_highest_cost_of_bike FROM bike_details WHERE Bike_Price < (SELECT MAX(Bike_Price) FROM bike_details);
+
+-- get the details of second largest cost of bike 
+
+SELECT*FROM bike_details WHERE Bike_Price = 
+(SELECT MAX(Bike_Price)AS Second_highest_cost_of_bike FROM bike_details WHERE Bike_Price < (SELECT MAX(Bike_Price) FROM bike_details));
+
+-- get third largest cost of book
+
+SELECT*FROM bike_details WHERE Bike_Price=100000;
+SELECT MAX(Bike_Price)AS Third_highest_cost_of_bike FROM bike_details WHERE Bike_Price < 
+(SELECT MAX(Bike_Price)AS Second_highest_cost_of_bike FROM bike_details WHERE Bike_Price < (SELECT MAX(Bike_Price) FROM bike_details));
+
+-- get the details of third largest cost of bike 
+
+SELECT*FROM bike_details WHERE Bike_Price = 
+(SELECT MAX(Bike_Price)AS Third_highest_cost_of_bike FROM bike_details WHERE Bike_Price < 
+(SELECT MAX(Bike_Price)AS Second_highest_cost_of_bike FROM bike_details WHERE Bike_Price < (SELECT MAX(Bike_Price) FROM bike_details)));
+
+-- get price between highest and second highest cost
+
+SELECT*FROM bike_details WHERE Bike_Price IN (100001,100000);
+SELECT*FROM bike_details WHERE Bike_Price IN((SELECT MAX(Bike_Price) FROM  bike_details),
+(SELECT MAX(Bike_Price)AS Second_highest_cost_of_bike FROM bike_details WHERE Bike_Price < (SELECT MAX(Bike_Price) FROM bike_details)));
+
+-- average cost
+
+SELECT AVG(Bike_Price) FROM bike_details;
+SELECT * FROM  bike_details WHERE Bike_Price IN (SELECT AVG(Bike_Price)FROM bike_details);
+
+-- get the bike details by the bike model which has bike id greater than 2
+
+SELECT * FROM bike_details WHERE Bike_Model IN((SELECT Bike_Model FROM bike_details GROUP BY Bike_Id HAVING Bike_Id>2));
+SELECT * FROM bike_details WHERE Bike_Id>1; 
 
 
 
